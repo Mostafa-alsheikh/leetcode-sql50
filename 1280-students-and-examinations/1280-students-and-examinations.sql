e798b14e8fbc396b40ab082c71fd679a918b8f3b
Mostafa-alsheikh <M.Mohamed2262@nu.edu.eg>
2026-07-14 04:15:45 +0300

    Time: 1329 ms (9.18%), Space: 0B (100%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,7 @@
+SELECT t1.student_id, t1.student_name, t2.subject_name, COUNT(t3.student_id) as attended_exams
+FROM Students as t1
+CROSS JOIN Subjects as t2
+LEFT JOIN Examinations as t3
+ON t1.student_id = t3.student_id AND t2.subject_name = t3.subject_name
+GROUP BY t1.student_id, t1.student_name, t2.subject_name
+ORDER BY t1.student_id, t2.subject_name
